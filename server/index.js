cbabeca4936c923c5577d91fd14a13dc3375528e
AMAN KUMAR <aman2110113@akgec.ac.in>
2024-01-24 21:57:10 +0530

    Added emit on socket

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 require('dotenv').config()
 const UserRouter = require('./routes/user')
-const  Server  = require("socket.io");
+const Server = require("socket.io");
+const Document = require("./models/document")
 
 
 const httpServer = createServer(app);
-const io =  Server(httpServer ,{
+const io = Server(httpServer, {
     cors: {
         origin: process.env.CORS_ORIGIN || "http://localhost:5173",
         methods: ["GET", "POST"],
@@ -18,9 +19,9 @@ const io =  Server(httpServer ,{
 
 // Mongo DB Connections
 mongoose.connect(process.env.MONGO_DB_URL, {
-}).then(response=>{
+}).then(response => {
     console.log('MongoDB Connection Succeeded.')
-}).catch(error=>{
+}).catch(error => {
     console.log('Error in DB connection: ' + error)
 });
 
@@ -32,7 +33,8 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
-app.use('/user' , UserRouter)
+app.use('/user', UserRouter)
+const defaultValue = "";
 
 //Socket Connection 
 try {
@@ -40,23 +42,41 @@ try {
         // ...
         console.log("Connected webSocket")
 
-        socket.on("send-changes" , delta=>{
-            // console.log(delta)
-            socket.broadcast.emit("recived-changes" , delta)
-        })
+        socket.on("get-document", documentId => {
+            const document = getDocumentOrCreate(documentId);
+            socket.join(documentId)
+            socket.emit("load-document", document.data)
 
+            socket.on("send-changes", delta => {
+                socket.broadcast.to(documentId).emit("recived-changes", delta)
+            })
+
+
+            socket.on("saved-document", async data => {
+                await Document.findByIdAndUpdate(documentId, { data })
+            })
+        })
         socket.on("disconnect", () => {
             console.log("Socket disconnected:", socket.id);
         });
-    
-      });
+
+    });
 } catch (error) {
-    console.warn("Unable to connect Websocket " , error)
+    console.warn("Unable to connect Websocket ", error)
 }
-  
+
 const PORT = process.env.PORT || 5000
 
 // Connection
-httpServer.listen(PORT, ()=>{
-    console.log('App running in port: '+PORT)
+httpServer.listen(PORT, () => {
+    console.log('App running in port: ' + PORT)
 })
+
+
+async function getDocumentOrCreate(id) {
+    if (id == null) return;
+    const data = await Document.findById(id);
+    if (data) return data;
+
+    return await Document.create({ _id: id, data: defaultValue })
+}
