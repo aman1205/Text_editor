bc62666044d0d177e84a4112741fb6bb3ec4613f
AMAN KUMAR <aman2110113@akgec.ac.in>
2024-01-26 12:18:19 +0530

    added redux persist

--- client/src/redux/authActions.js
+++ client/src/redux/authActions.js
@@ -1,13 +1,12 @@
 export const login = (userId) => {
-    localStorage.setItem('userId', userId);
     return {
       type: 'LOGIN',
       payload: userId,
     };
   };
   
+ 
   export const logout = () => {
-    localStorage.removeItem('userId');
     return {
       type: 'LOGOUT',
     };
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,25 @@
 // src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from "../redux/authReducer"
-const store = configureStore({
-  reducer: authReducer,
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-    serializableCheck:false,
+import { persistReducer, persistStore} from "redux-persist"
+import storage from 'redux-persist/lib/storage'
+
+
+
+const rootReducer = combineReducers({ authReducer });
+
+const  persistConfig = {
+  key: "userRoot",
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
   })
 });
 
-export default store;
+export  const  persistor = persistStore(store)
